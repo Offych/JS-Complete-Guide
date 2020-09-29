485445b6efeaadee57b8ab98a987c96c5b8f8241
Offych <off@tut.by>
2020-09-29 09:47:05 +0300

    Sets and Maps

---
+++ Arrays and Iterables/setsmaps.js
@@ -0,0 +1,32 @@
+/* const ids = new Set(["hu", "HU1", "123"]);
+ids.add(2);
+ids.delete("hu");
+
+console.log(ids);
+
+for (const entry of ids.entries()) {
+  console.log(entry[0]);
+}
+ */
+const person1 = { name: "alex" };
+const person2 = { name: "polina" };
+
+const personData = new Map([[person1, [{ date: "monday", price: 10 }]]]);
+
+personData.set(person2, [{ date: "2 weeks", price: 50 }]);
+
+console.log(personData);
+
+console.log(personData.get(person1));
+
+for (const [key, value] of personData.entries()) {
+  console.log(key, value);
+}
+
+for (const key of personData.keys()) {
+  console.log(key);
+}
+for (const value of personData.values()) {
+  console.log(value);
+}
+console.log(personData.size);
